324bfd334911f0f03a84e51a0ee5cd4d19d3e114
Simon Dalvai <s.dalvai@noi.bz.it>
2022-02-14 17:15:25 +0100

    max value for cars radial progress working

--- code/components/details.js
+++ code/components/details.js
@@ -31,10 +31,14 @@ export function render_details() {
     let brandName = cars[station].smetadata.brand;
     let availability = cars[station]["sdatatypes"]["availability"]["tmeasurements"][0]["mvalue"] === 0 ? 1 : 0;
     if(brandName in carByBrands){
-      carByBrands[brandName] += availability;
+      carByBrands[brandName]["availability"] += availability;
+      carByBrands[brandName]["maxAvailability"] += 1;
+
     } else {
-      carByBrands[brandName] = 0;
-      carByBrands[brandName] += availability;
+      carByBrands[brandName] = {};
+      carByBrands[brandName]["availability"] = 0;
+      carByBrands[brandName]["maxAvailability"] = 1;
+      carByBrands[brandName]["availability"] += availability;
     }
   }
 
@@ -42,10 +46,12 @@ export function render_details() {
 
   for (let key in carByBrands) {
     let brandName = key;
-    let availability = carByBrands[key];
+    let availability = carByBrands[key]["availability"];
+    let maxAvailability = carByBrands[key]["maxAvailability"];
+
     carDetails.push(html`<wc-radial-progress
     .minValue=0
-    .maxValue=2
+    .maxValue=${maxAvailability}
     .value=${availability}
     .width=${swidth}
     .height=${sheight}
