6f48cbfe2c4f76fb38822b90e6853820fb76739f
Simon Dalvai <s.dalvai@noi.bz.it>
2022-02-18 15:33:23 +0100

    revert isMobile

--- code/utils.js
+++ code/utils.js
@@ -6,7 +6,7 @@ export const LANGUAGES = {
 };
 
 export const isMobile = () => {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return document.body.offsetWidth < 992;
 };
 
 export function getCurrentPosition(options = {}) {
